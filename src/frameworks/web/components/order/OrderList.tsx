170fb21deb64d08bd79d34ccf346c7e9b6bbb5cc
Tommy <tommy@antigravity.kr>
2023-02-23 15:40:41 +0900

    feat: 메인페이지에서 주문내역 출력

---
+++ src/frameworks/web/components/order/OrderList.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import OrderItem, {OrderItemInfo} from "@frameworks/web/components/order/OrderItem";
+import {observer} from "mobx-react";
+import {useEffect} from "react";
+
+const OrderList: React.FC = () => {
+  const vm = orderListVm;
+  const list = vm.orderList;
+
+  useEffect(() => {
+    (async () => {
+      await vm.getOrderList();
+    })();
+  }, []);
+
+  return (
+    <div>
+      {
+        list.map((item, index) => {
+          const itemInfo: OrderItemInfo = {
+            amount: item.amount,
+            id: item.id,
+            orderAt: item.orderAt,
+            shippingList: item.shippingList
+          };
+          return (
+            <OrderItem
+              key={index}
+              amount={item.amount}
+              id={item.id}
+              orderAt={item.orderAt}
+              shippingList={item.shippingList}
+            />
+          );
+        })
+      }
+    </div>
+  )
+}
+
+export default observer(OrderList);
